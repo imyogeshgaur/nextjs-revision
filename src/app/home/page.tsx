95432512584c9673be4be2ae3091b08b16bf4aa6
imyogeshgaur <yogesh.gaur@carrier.com>
2024-04-03 10:57:14 +0530

    Rendered cards of users in home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,48 @@
-"use client"
-import React from 'react'
+"use client";
+import Card from "@/Components/Card";
+import NavBar from "@/Components/NavBar";
+import { GET_ALL_USERS_URL } from "@/constants/Constants";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-  return (
-    <div> Home</div>
-  )
-}
+  const routes = useRouter();
+  const [users, setUsers] = useState([]);
 
-export default Home
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token == undefined || null || "") {
+      routes.push("/");
+    }
+    axios
+      .get(GET_ALL_USERS_URL, {
+        headers: {
+          Authorization: token,
+        },
+      })
+      .then((responseFromBackend: any) =>
+        setUsers(responseFromBackend.data.users)
+      );
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+      <div className="flex flex-wrap">
+      {users.map((val: any, index: number) => {
+        return (
+          <Card
+            key={index}
+            name={val.nameOfUser}
+            email={val.emailOfUser}
+            phone={val.phoneNumber}
+          />
+        );
+      })}
+      </div>
+    </>
+  );
+};
+
+export default Home;
