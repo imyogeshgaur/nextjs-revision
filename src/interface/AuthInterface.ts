8f0838b5b11ad649b9a48a011f7f8b3b341dacb6
imyogeshgaur <yogesh.gaur@carrier.com>
2024-04-03 10:05:24 +0530

    Added some interfaces

---
+++ src/interface/AuthInterface.ts
@@ -0,0 +1,11 @@
+export interface LoginInterface{
+    emailOrPhone:string;
+    password:string;
+}
+
+export interface RegisterInterface{
+    nameOfUser:string;
+    emailOfUser:string;
+    phoneNumber:string;
+    password:string;
+}
